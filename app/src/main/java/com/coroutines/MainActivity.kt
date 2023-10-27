e91bacb09de6eee607a2eabf57703c65762829a5
Umang Rathod <umang00775@gmail.com>
2023-10-27 16:42:35 +0530

    Dependecy added + First coroutine started

--- app/src/main/java/com/coroutines/MainActivity.kt
+++ app/src/main/java/com/coroutines/MainActivity.kt
@@ -2,10 +2,26 @@ package com.coroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /* Start a coroutine */
+        /* This is not the best way to start a coroutine */
+        GlobalScope.launch {
+            delay(3000L) /* Pause the thread for 3 seconds */
+            Log.d(TAG, "Hello, from ${Thread.currentThread().name}")
+        }
+        Log.d(TAG, "Hello, from ${Thread.currentThread().name}")
     }
 }
