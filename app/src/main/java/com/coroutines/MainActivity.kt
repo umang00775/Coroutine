b16953fd2b1f31062fb67f5bc67e84821d2c7f19
Umang Rathod <umang00775@gmail.com>
2023-10-28 22:25:52 +0530

    Parallel threads working

--- app/src/main/java/com/coroutines/MainActivity.kt
+++ app/src/main/java/com/coroutines/MainActivity.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
@@ -21,21 +22,48 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val job = GlobalScope.launch(Dispatchers.Default) {
-            for (i in 30..40){
-                Log.d(TAG, "Fibonacci for $i = ${fib(i)}") // Ex. of fibonacci because it takes much time
+
+        /* This will take 6 seconds */
+        GlobalScope.launch {
+            val time = measureTimeMillis {
+                val response1 = networkCall1()
+                val response2 = networkCall2()
+                Log.d(TAG, response1)
+                Log.d(TAG, response2)
             }
+            Log.d(TAG, "$time")
         }
 
-        runBlocking {
-            delay(3000L)
-            job.cancel()
-            Log.d(TAG, "Running ${Thread.currentThread().name}")
+        Log.d(TAG, "\n\n")
+
+        /* This will take 3 seconds for the same operations as above */
+        GlobalScope.launch {
+            val time = measureTimeMillis {
+                var response1: String? = null
+                var response2: String? = null
+
+                val job1 = launch { response1 = networkCall1() }
+                val job2 = launch { response2 = networkCall1() }
+
+                job1.join()
+                job2.join()
+
+                Log.d(TAG, "$response1")
+                Log.d(TAG, "$response2")
+            }
+            Log.d(TAG, "$time")
         }
+
+
     }
 
-    fun fib(num: Int): Int{
-        if (num <= 1) return num
-        return fib(num-1) + fib(num-2)
+    private suspend fun networkCall1(): String{
+        delay(3000L)
+        return "Response from server - 1"
+    }
+
+    private suspend fun networkCall2(): String {
+        delay(3000L)
+        return "Response from server - 2"
     }
 }
