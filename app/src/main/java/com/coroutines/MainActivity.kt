ed03abc06ecd529f118239850d4df01e00aa8e5d
Umang Rathod <umang00775@gmail.com>
2023-10-29 22:25:46 +0530

    Firestore completed - but code throwing some errors

--- app/src/main/java/com/coroutines/MainActivity.kt
+++ app/src/main/java/com/coroutines/MainActivity.kt
@@ -8,6 +8,9 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.google.firebase.Firebase
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.firestore
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -15,6 +18,7 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 import kotlin.system.measureTimeMillis
 
@@ -26,36 +30,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.button)
+        val tv = findViewById<TextView>(R.id.textview)
 
-        button.setOnClickListener {
-            /* This will run while the app is alive, which can cause a memory leaks */
-            /* Hence we use lifecycle for this, It will run while the Activity is alive */
-            GlobalScope.launch {
-                /* This will continue running even we finish this and start another activity */
-                while (true){
-                    delay(1000L)
-                    Log.d(TAG, "Still running ${Thread.currentThread().name}")
-                }
+        FirebaseApp.initializeApp(this@MainActivity)
+
+        val document = Firebase.firestore
+            .collection("document")
+            .document("people")
+
+        val data = Person("Umang", 20)
+
+        GlobalScope.launch(Dispatchers.IO) {
+            delay(3000L)
+
+            document.set(data).await()  /* Uploading the document and wait */
+            val umang = document.get().await().toObject(Person::class.java) /* Get the data */
+            withContext(Dispatchers.Main){
+                tv.text = umang.toString()
             }
 
-            /* This will run, only when this activity is alive */
-            lifecycleScope.launch {
-                while (true){
-                    delay(1000L)
-                    Log.d(TAG, "Running ${Thread.currentThread().name}")
-                }
-            }
-
-            GlobalScope.launch {
-                delay(5000L)
-                Intent(this@MainActivity, SecondActivity::class.java).also {
-                    startActivity(it)
-                    finish()  /* Even after finish the upper while loop continues to run */
-                }
-
-            }
         }
 
     }
 }
+
+data class Person(val name: String, val age: Int){}
